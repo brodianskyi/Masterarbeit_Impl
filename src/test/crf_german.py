c94afd03a3471a05b4e7fa0fc1019b4fc82197cd
rody <brodianskyi@gmail.com>
2019-05-12 12:07:16 +0200

    add lambda function for tags processing

--- src/test/crf_german.py
+++ src/test/crf_german.py
@@ -5,10 +5,7 @@ import pandas as pd
 data = pd.read_csv("NER-de-train.tsv", names=["Word_number", "Word", "OTR_Span", "EMB_Span", "Sentence_number"],
                    delimiter="\t",
                    quoting=csv.QUOTE_NONE, encoding='utf-8')
-data = data.fillna(method="ffill")
 data = data.head(300)
-# print(data)
-# print("Number of sentences: ", len(data.groupby(["Sentence"])))
 
 words = list(set(data["Word"].values))
 n_word = len(words)
@@ -26,45 +23,48 @@ emb_tags = len(emb_tags)
 
 # print("Number of emb_tags", emb_tags)
 
+
 class SentenceGetter(object):
 
-    def prepare_data(self):
-        for i in self.data["Sentence"].values:
-            if i == self.sentence_begin:
-                print("Beginning of the sentence")
-
     def __init__(self, data):
-        self.sentence_info = list()
+        self.sentences_info = list()
         self.sentences = list()
-        self.main_list = list()
-        self.n_sent = 1
+        self.grouped = list()
         self.data = data
         self.empty = False
-        sentence_count = 0
-        step_count = -1
+        self.sentences_count = 0
+        self.step_count = -1
 
         for i in self.data["Word_number"].values:
             # print("i = ", i)
-            step_count += 1
+            self.step_count += 1
             if i == "#":
-                # print("--------------sentence begin")
-                sentence_count += 1
-                self.data.at[step_count, "Sentence_number"] = sentence_count
-                # print("Sentence: {}".format(sentence_count))
-                self.sentence_info.append((self.data.at[step_count, "Word"],
-                                           self.data.at[step_count, "OTR_Span"]))
+                self.sentences_count += 1
+                self.data.at[self.step_count, "Sentence_number"] = self.sentences_count
+                self.data.at[self.step_count, "EMB_Span"] = 0
+                self.sentences_info.append((self.data.at[self.step_count, "Word"],
+                                            self.data.at[self.step_count, "OTR_Span"]))
             else:
-                # print("step_count=", step_count)
-                # print("ppp", self.data.at[step_count, "Word"])
-                self.data.at[step_count, "Sentence_number"] = sentence_count
-                self.sentences.append((self.data.at[step_count, "Word"],
-                                       self.data.at[step_count, "OTR_Span"],
-                                       self.data.at[step_count, "EMB_Span"]))
-            # print(self.sentence_info)
-            # print(self.grouped)
+                self.data.at[self.step_count, "Sentence_number"] = self.sentences_count
 
-        self.main_list = [s for s in self.data.groupby("Sentence_number")]
-        print(self.main_list)
+            agg_func = lambda s: [(w, p, t) for w, p, t in zip(s["Word"].values.tolist(),
+                                                               s["OTR_Span"].values.tolist(),
+                                                               s["EMB_Span"].values.tolist())]
+
+        self.grouped = self.data.groupby("Sentence_number").apply(agg_func)
+        self.sentences = [s for s in self.grouped]
+        self.sentences_count = 0
+
+    def get_first_sent(self):
+        try:
+            s = self.sentences[self.sentences_count]
+            self.sentences_count += 1
+            return s
+        except:
+            return None
 
 
 getter = SentenceGetter(data)
+sent = getter.get_first_sent()
+sentences = getter.sentences
+print("sentences", sentences)
