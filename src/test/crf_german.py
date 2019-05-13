99a46d41199b1a02a24b3ae3a371dfed5da81791
pasha <brodianskyi@gmail.com>
2019-05-13 12:20:58 +0200

    add training and predicting phases for the model

--- src/test/crf_german.py
+++ src/test/crf_german.py
@@ -5,7 +5,10 @@ import numpy as np
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
+from keras.models import Model, Input
+from keras.layers import LSTM, Embedding, Dense, TimeDistributed, Dropout, Bidirectional
 from keras_contrib.layers import CRF
+from sklearn_crfsuite.metrics import flat_classification_report
 
 # number of examples used in each iteration
 BATCH_SIZE = 512
@@ -131,6 +134,32 @@ X_tr.shape, X_te.shape, np.array(y_tr).shape, np.array(y_te).shape
 
 print("Raw Sample: ", " ".join([w[0] for w in sentences[0]]))
 print("Raw Label: ", " ".join([w[1] for w in sentences[0]]))
+
+# Model definition
+input = Input(shape=(MAX_LEN, ))
+# input_dim = n_word + PAD + UNK
+model = Embedding(input_dim=n_words + 2, output_dim=EMBEDDING, input_length=MAX_LEN, mask_zero=True)(input)
+model = Bidirectional(LSTM(units=50, return_sequences=True, recurrent_dropout=0.1))(model)
+model = TimeDistributed(Dense(50, activation="relu"))(model)
+# CRF Layer = n_otr_tags + PAD
+crf = CRF(n_otr_tags+1)
+out = crf(model)
+model = Model(input, out)
+model.compile(optimizer="rmsprop", loss=crf.loss_function, metrics=[crf.accuracy])
+model.summary()
+# training
+history = model.fit(X_tr, np.array(y_tr), batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=0.1, verbose=2)
+pred_cat = model.predict(X_te)
+pred = np.argmax(pred_cat, axis=-1)
+y_te_true = np.argmax(y_te, -1)
+# convert the index to tag
+pred_tag = [[idx2tag[i] for i in row] for row in pred]
+y_te_true_tag = [[idx2tag[i] for i in row] for row in y_te_true]
+print("pred_tag", pred_tag)
+print("y_te_true_tag", y_te_true_tag)
+
+report = flat_classification_report(y_pred=pred_tag, y_true=y_te_true_tag)
+#print(report)
 
 
 
